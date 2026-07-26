7ec6239b6ba61f005e1d13e4198ca1163b02da87
mayesamollah-ctrl <mayesamollah@gmail.com>
2026-07-27 03:35:48 +0530

    Update Help.js

--- scripts/cmds/Help.js
+++ scripts/cmds/Help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: 𝐌𝐀𝐌𝐔𝐍 彡\n├‣ WHATSAPP\n╰‣ 01830981279`;
+                        msg += `\n╭─✦ ADMIN: ♡┋Habibᥫ᭡\n├‣ WHATSAPP\n╰‣ +918327378209`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
