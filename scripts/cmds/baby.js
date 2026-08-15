dd37c9a290cdd9398529d8b80033eaf17e29bb3f
SkHabib-Goat-V2 <sksala334@gmail.com>
2026-08-16 03:24:49 +0530

    Update baby.js

--- scripts/cmds/baby.js
+++ scripts/cmds/baby.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const baseApiUrl = async () => {
-    return "https://noobs-api.top/dipto";
+    return "https://simsimi-xdi.onrender.com";
 };
 
 module.exports.config = {
